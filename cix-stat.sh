c1b67e583ea6525f0a8fdaf4764bef0584edc64e
Hellseher <sharlatanus@gmail.com>
2016-11-26 22:06:31 +0000

    [E] Modified output

--- cix-stat.sh
+++ cix-stat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # File          : cix-stat.sh
 # Created       : <2016-11-20 Sun 21:59:18 GMT> sharlatan
-# Last Modified : <2016-11-20 Sun 23:58:42 GMT> sharlatan
+# Last Modified : <2016-11-26 Sat 22:04:30 GMT> sharlatan
 # Author        : sharlatan <sharlatanus@gmail.com>
 # Short :
 
@@ -10,11 +10,12 @@ ABSPATH="$(dirname $(readlink -f $0))"
 
 ls_cmd_distribution() {
     # List how many examplas of command in project.
-    find "$ABSPATH"/spices -type f -name "*org" \
-         -exec grep -oP "(?<=-\s).+(?=-[0-9]{12})" {} \; \
-        | sort \
-        | uniq -c \
-        | sort -nr
+    (echo "QTY CMD" ;
+     find "$ABSPATH"/spices -type f -name "*org" \
+          -exec grep -oP "(?<=-\s).+(?=-[0-9]{12})" {} \; \
+         | sort \
+         | uniq -c \
+         | sort -nr) | column -t
 }
 
 main() {
