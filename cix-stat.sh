c33efa2b97604b460f167a780d6326a2331d3aaa
Hellseher <sharlatanus@gmail.com>
2017-04-16 21:40:33 +0100

    [E] Some refuctorings

--- cix-stat.sh
+++ cix-stat.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-# File          : cix-stat.sh
-# Created       : <2016-11-20 Sun 21:59:18 GMT> sharlatan
-# Last Modified : <2016-12-01 Thu 22:13:54 GMT> sharlatan
-# Author        : sharlatan <sharlatanus@gmail.com>
-# Short :
+# File     : cix-stat.sh
+# Created  : <2016-11-20 Sun 21:59:18 GMT> sharlatan
+# Modified : <2017-4-16 Sun 12:01:44 BST> sharlatan
+# Author   : sharlatan <sharlatanus@gmail.com>
+# Short    :
 
 
-ABSPATH="$(dirname $(readlink -f $0))"
+ABS_PATH="$(dirname "$(readlink -f "$0")")"
 
-ls_cmd_distribution() {
-    # List how many examplas of command in project.
-    (echo "QTY CMD" ;
-     find "$ABSPATH"/spices -type f -name "*org" \
-          -exec grep -oP "(?<=\*\*\*\*\s).+(?=-[0-9]{12})" {} \; \
-         | sort \
-         | uniq -c \
-         | sort -nr) | column -t
+ls_cmd_distribution()
+{ # List how many examplas of command in project.
+    { echo "QTY CMD" ;
+      find "$ABS_PATH"/spices -type f -name "*org" \
+           -exec grep -oP "(?<=\*\*\*\*\s).+(?=-[0-9]{12})" {} \; \
+          | sort \
+          | uniq -c \
+          | sort -nr; } | column -t
 }
 
 main() {
