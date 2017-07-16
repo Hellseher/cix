fae182498f7371d0794db6c8c6cd08c51e29c051
Hellseher <sharlatanus@gmail.com>
2017-07-17 00:52:26 +0100

    [E] Update cix-stat.sh
    Added PKG column to output. Show statistic about oneliners and scripts, but it
    needs to be completed.

--- cix-stat.sh
+++ cix-stat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # File     : cix-stat.sh
 # Created  : <2016-11-20 Sun 21:59:18 GMT> sharlatan
-# Modified : <2017-6-19 Mon 22:10:57 BST> sharlatan
+# Modified : <2017-7-17 Mon 00:51:39 BST> sharlatan
 # Author   : sharlatan <sharlatanus@gmail.com>
 # Short    : Show statistic of examples.
 
@@ -10,16 +10,35 @@ ABS_PATH="$(dirname "$(readlink -f "$0")")"
 
 ls_cmd_distribution()
 { # List how many examplas of command in project.
-    { echo "QTY CMD" ;
-      find "$ABS_PATH"/spices -type f -name "*org" \
-           -exec grep -oP "(?<=\*\*\*\*\s).+(?=-[0-9]{12})" {} \; \
-          | sort \
-          | uniq -c \
-          | sort -nr; } | column -t
+    { echo "QTY CMD PKG" ;
+      echo --- --- ---;
+      [ -e "$ABS_PATH"/spices ] &&
+          find "$ABS_PATH"/spices -type f -name "*org"  \
+               -exec  grep -oHP "(?<=\*\*\*\*\s).+(?=-[0-9]{12})" {} \; \
+              | sed -e 's/^\/.*cix-//g' \
+              | awk -F":" '{print $2," ",$1}' \
+              | sed -e 's/\.org//g' \
+              | sort \
+              | uniq -c \
+              | sort -nr;
+    } | column -t
+}
+
+oneliner_or_script()
+{ # Count oneliner and script in the project.
+    { printf "Onliners: ";
+      find "$ABS_PATH"/spices -type f -name "*org" -print0 \
+          | xargs -0 grep -oP "^:\s+~" | wc -l;
+      printf "Scripts: ";
+      find "$ABS_PATH"/spices -type f -name "*org" -print0 \
+          | xargs -0 grep -o "\#+BEGIN_SRC" | wc -l;
+    } | column -t
 }
 
 main() {
     ls_cmd_distribution
+ #   echo
+  #  oneliner_or_script
 }
 
 main "$@"
